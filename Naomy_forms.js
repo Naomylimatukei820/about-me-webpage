70d5f661387fd3d3b00a82a74216a47571940221
Naomy <naomylimatukei@yahoo.com>
2025-05-17 21:47:09 -0500

    Add files via upload

---
+++ Naomy_forms.js
@@ -0,0 +1,103 @@
+/Perform arithmetic on two numbers from the user
+
+function performArithmetic() {
+  //Get the two numbers and operator from form and store them
+  const firstarithmeticnumber = Number(document.forms.ArithmeticForm.firstarithmeticnumber.value);
+  const secondarithmeticnumber = Number(document.forms.ArithmeticForm.secondarithmeticnumber.value);
+  const arithmeticoperator = document.forms.ArithmeticForm.arithmeticoperator.value;
+  let arithmeticresult = 0;
+
+  //alert('The result of adding ${firstarithmeticnumber} and ${secondarithmeticnumber} is ${arithmeticresult}.');
+
+  //Switch statement evaluating on chosen Arithmetic Operator
+  switch(arithmeticoperator) {
+    case 'Addition':
+      arithmeticresult = firstarithmeticnumber + secondarithmeticnumber;
+      alert('The result of adding ' + firstarithmeticnumber + ' and ' + secondarithmeticnumber + ' is ' + arithmeticresult + '.');
+      break;
+    case 'Subtraction':
+      arithmeticresult = firstarithmeticnumber - secondarithmeticnumber;
+      alert('The result of subtracting ' + firstarithmeticnumber + ' and ' + secondarithmeticnumber + ' is ' + arithmeticresult + '.');
+      break;
+    case 'Multiplication':
+      arithmeticresult = firstarithmeticnumber * secondarithmeticnumber;
+      alert('The result of multiplying ' + firstarithmeticnumber + ' and ' + secondarithmeticnumber + ' is ' + arithmeticresult + '.');
+      break;
+    case 'Division':
+      if (secondarithmeticnumber === 0) {
+        alert('Cannot divide by zero!');
+      } else {
+        arithmeticresult = firstarithmeticnumber / secondarithmeticnumber;
+        alert('The result of dividing ' + firstarithmeticnumber + ' and ' + secondarithmeticnumber + ' is ' + arithmeticresult + '.');
+      }
+      break;
+    default:
+      // Unnecessary since Addition is the default selection
+      alert('Default.');
+      break;
+  }
+}
+
+//Question 2
+//Calculate the factorial of a number
+function performFactorial() {
+  //Get the number from the form and store it
+  const factorialnumber = Number(document.forms.FactorialForm.factorialnumber.value);
+  let factorialresult = 0;
+
+  //alert('The factorial of ${factorialnumber} is ${factorialresult}.');
+
+  if (factorialnumber < 0) {
+    alert('Cannot calculate the factorial of a negative number!');
+    return;
+  } else if (factorialnumber === 0) {
+    alert('The factorial of 0 is 1.');
+    return;
+  } else {
+    factorialresult = 1;
+    for (i = 1; i <= factorialnumber; i++) {
+      factorialresult *= i;
+    }
+  }
+  alert('The factorial of ' + factorialnumber + ' is ' + factorialresult + '.');
+}
+
+//Question 3
+//Calculate the nth Fibonacci number
+function performFibonacci() {
+  //Get the number from the form and store it
+  const fibonaccinumber = Number(document.forms.FibonacciForm.fibonaccinumber.value);
+  let num1 = 0;
+  let num2 = 1;
+  let result = 0;
+
+  if (fibonaccinumber < 0) {
+    alert('Cannot calculate the nth Fibonacci number of a negative number!');
+  } else if (fibonaccinumber === 0) {
+    alert('The nth Fibonacci number for 0 is 0.');
+    return;
+  } else {
+    for (i = 2; i <= fibonaccinumber; i++) {
+      result = num1 + num2;
+      num1 = num2;
+      num2 = result;
+    }
+  }
+  alert('The nth Fibonacci number for ' + fibonaccinumber + ' is ' + result + '.');
+}
+
+//Question 4
+//Take three numbers, find the max and min and calculate the range
+function performRange() {
+  //Get the number from the form and store it
+  const num1 = Number(document.forms.RangeForm.num1.value);
+  const num2 = Number(document.forms.RangeForm.num2.value);
+  const num3 = Number(document.forms.RangeForm.num3.value);
+  let range = 0;
+
+  let max = Math.max(num1, num2, num3);
+  let min = Math.min(num1, num2, num3);
+  range = max - min;
+
+  alert('The range between ' + min + ' and ' + max + ' is ' + range + '.');
+}
